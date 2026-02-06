4bd1f09d97dca99b786d9b21971b98326f73ea09
sseobi16 <jseobi3951@naver.com>
2026-02-07 00:04:42 +0900

    Feat: 날짜 포멧 맞추기

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import re
 
 def get_today_str():
     # 현재 날짜를 YYYYMMDD 문자열로 반환
@@ -12,6 +13,9 @@ def clean_text(text):
     # 텍스트 전처리 함수
     if not text:
         return ""
+    
+    date_pattern = re.compile(r'(\d{4}/\d{2}/\d{2})(\d{2}:\d{2})')
+    text = date_pattern.sub(r'\1 \2', text)
     
     # 공백 제거 및 줄바꿈 문자를 공백으로 치환
     return text.strip().replace("\n", " ").replace("\r", "")
